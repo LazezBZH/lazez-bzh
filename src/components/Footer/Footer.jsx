cb96662fac6afeb3137d5fc11c3817bfaebc7fe8
LazezBZH <lazez.bzh@caramail.fr>
2021-11-11 16:39:19 +0100

    V06

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,20 @@
 import Links from "../Links/Links";
 import "./Footer.css";
+import react from "../../assets/react.png";
+import netlify from "../../assets/netlify.png";
 
 export default function Footer() {
   return (
     <div className="footer">
       <div className="footerLinks">
         <Links />
+      </div>
+      <div className="footerTxt">
+        <p>Portfolio créé sous React</p> <img src={react} alt="" />
+        <p>et hébergé par</p>
+        <a href="https://www.netlify.com/">
+          <img src={netlify} alt="" />
+        </a>
       </div>
     </div>
   );
