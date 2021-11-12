3d359296f3070acdde2478685b04fd94429c453a
LazezBZH <lazez.bzh@caramail.fr>
2021-11-12 11:03:01 +0100

    V09

--- src/pages/Cv/Cv.jsx
+++ src/pages/Cv/Cv.jsx
@@ -1,9 +1,12 @@
 import Banner from "../../components/Banner/Banner";
+import Footer from "../../components/Footer/Footer";
 
 export default function Cv() {
   return (
     <div className="home">
       <Banner />
+      <h1>à venir</h1>
+      <Footer />
     </div>
   );
 }
