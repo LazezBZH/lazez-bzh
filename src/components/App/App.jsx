8f730514d33e2083ec4f8f967d01d890a53438d6
LazezBZH <lazez.bzh@caramail.fr>
2022-01-12 12:40:13 +0100

    V15

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,21 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-//import Home from "../../pages/Home/Home";
-//import Cv from "../../pages/Cv/Cv";
+import Home from "../../pages/Home/Home";
+import Cv from "../../pages/Cv/Cv";
 import "./App.css";
 
-const Home = lazy(() => import("../../pages/Home/Home"));
-const Cv = lazy(() => import("../../pages/Cv/Cv"));
+//const Home = lazy(() => import("../../pages/Home/Home"));
+//const Cv = lazy(() => import("../../pages/Cv/Cv"));
 
 function App() {
   return (
     <React.StrictMode>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/cv" component={Cv} />
-          </Switch>
-        </Router>
-      </Suspense>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/cv" component={Cv} />
+        </Switch>
+      </Router>
     </React.StrictMode>
   );
 }
--- src/pages/Cv/Cv.jsx
+++ src/pages/Cv/Cv.jsx
@@ -1,12 +1,28 @@
 import Banner from "../../components/Banner/Banner";
-import Footer from "../../components/Footer/Footer";
+//import Footer from "../../components/Footer/Footer";
+import cvjanv from "../../assets/cv janv 22.png";
+import "./Cv.css";
 
 export default function Cv() {
   return (
-    <div className="home">
-      <Banner />
-      à venir
-      <Footer />
+    <div className="cv">
+      <div className="cvBanner">
+        <Banner />
+      </div>
+      <div class="image-zoom">
+        <input type="checkbox" id="zoomCheck"></input>
+        <label for="zoomCheck">
+          <img className="cvImg" src={cvjanv} alt="" />
+        </label>
+      </div>
+      <button className="download">
+        <a
+          href="../../assets/cv janv 22.pdf"
+          download="CV Bénédicte HÉRAULT développeuse fonrt-end.pdf"
+        >
+          Téléchargez mon CV en PDF
+        </a>
+      </button>
     </div>
   );
 }
