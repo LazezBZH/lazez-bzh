ee72df0e69af44d52655c93ca05f19821c95f0a5
LazezBZH <lazez.bzh@caramail.fr>
2022-05-09 17:07:29 +0200

    V32

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,17 +1,20 @@
 import banner from "../../assets/web.jpg";
 import moi from "../../assets/moi2.jpg";
+import { Link } from "react-router-dom";
 import "./Banner.css";
 export default function Banner() {
   return (
     <div className="banner">
-      <img className="bannerImg" src={banner} alt="" />
-      <div className="bannerTxt">
-        <p className="name">Bénédicte HÉRAULT</p>
-        <p className="job">Développeuse front-end</p>
-      </div>
-      <div className="moi">
-        <img className="moiImg" src={moi} alt="" />
-      </div>
+      <Link to="/">
+        <img className="bannerImg" src={banner} alt="" />
+        <div className="bannerTxt">
+          <p className="name">Bénédicte HÉRAULT</p>
+          <p className="job">Développeuse front-end</p>
+        </div>
+        <div className="moi">
+          <img className="moiImg" src={moi} alt="" />
+        </div>
+      </Link>
     </div>
   );
 }
